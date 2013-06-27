5c176ba1205d8ed104d98a2e8ea099b59780277b
Adam Peacock (work) <adam@thepeacock.net>
2013-06-27 09:50:02 -0400

    Moved page and content to South migrations

---
+++ migrate/page/0001_initial.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Page'
+        db.create_table(u'page_page', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('lft', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+            ('rght', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+            ('tree_id', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+            ('level', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+            ('active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=150)),
+            ('parent', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='children', null=True, to=orm['page.Page'])),
+            ('in_navigation', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('override_url', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('redirect_to', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('_cached_url', self.gf('django.db.models.fields.CharField')(default='', max_length=255, db_index=True, blank=True)),
+            ('navigation_extension', self.gf('django.db.models.fields.CharField')(max_length=200, null=True, blank=True)),
+            ('publication_date', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 6, 27, 0, 0))),
+            ('publication_end_date', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('_content_title', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('_page_title', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
+            ('template_key', self.gf('django.db.models.fields.CharField')(default='basic.html', max_length=255)),
+        ))
+        db.send_create_signal(u'page', ['Page'])
+
+        # Adding model 'RichTextContent'
+        db.create_table(u'page_page_richtextcontent', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('text', self.gf('feincms.contrib.richtext.RichTextField')(blank=True)),
+            ('parent', self.gf('django.db.models.fields.related.ForeignKey')(related_name='richtextcontent_set', to=orm['page.Page'])),
+            ('region', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('ordering', self.gf('django.db.models.fields.IntegerField')(default=0)),
+        ))
+        db.send_create_signal(u'page', ['RichTextContent'])
+
+        # Adding model 'BiographyContent'
+        db.create_table(u'page_page_biographycontent', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('section', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['content.BiographySection'])),
+            ('parent', self.gf('django.db.models.fields.related.ForeignKey')(related_name='biographycontent_set', to=orm['page.Page'])),
+            ('region', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('ordering', self.gf('django.db.models.fields.IntegerField')(default=0)),
+        ))
+        db.send_create_signal(u'page', ['BiographyContent'])
+
+        # Adding model 'CalendarContent'
+        db.create_table(u'page_page_calendarcontent', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('calendar_src', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('parent', self.gf('django.db.models.fields.related.ForeignKey')(related_name='calendarcontent_set', to=orm['page.Page'])),
+            ('region', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('ordering', self.gf('django.db.models.fields.IntegerField')(default=0)),
+        ))
+        db.send_create_signal(u'page', ['CalendarContent'])
+
+        # Adding model 'ImageContent'
+        db.create_table(u'page_page_imagecontent', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=255)),
+            ('alt_text', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('caption', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('parent', self.gf('django.db.models.fields.related.ForeignKey')(related_name='imagecontent_set', to=orm['page.Page'])),
+            ('region', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('ordering', self.gf('django.db.models.fields.IntegerField')(default=0)),
+            ('position', self.gf('django.db.models.fields.CharField')(default='left', max_length=10)),
+        ))
+        db.send_create_signal(u'page', ['ImageContent'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Page'
+        db.delete_table(u'page_page')
+
+        # Deleting model 'RichTextContent'
+        db.delete_table(u'page_page_richtextcontent')
+
+        # Deleting model 'BiographyContent'
+        db.delete_table(u'page_page_biographycontent')
+
+        # Deleting model 'CalendarContent'
+        db.delete_table(u'page_page_calendarcontent')
+
+        # Deleting model 'ImageContent'
+        db.delete_table(u'page_page_imagecontent')
+
+
+    models = {
+        u'content.biographysection': {
+            'Meta': {'object_name': 'BiographySection'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'page.biographycontent': {
+            'Meta': {'ordering': "['ordering']", 'object_name': 'BiographyContent', 'db_table': "u'page_page_biographycontent'"},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ordering': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'biographycontent_set'", 'to': u"orm['page.Page']"}),
+            'region': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'section': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['content.BiographySection']"})
+        },
+        u'page.calendarcontent': {
+            'Meta': {'ordering': "['ordering']", 'object_name': 'CalendarContent', 'db_table': "u'page_page_calendarcontent'"},
+            'calendar_src': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ordering': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'calendarcontent_set'", 'to': u"orm['page.Page']"}),
+            'region': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'page.imagecontent': {
+            'Meta': {'ordering': "['ordering']", 'object_name': 'ImageContent', 'db_table': "u'page_page_imagecontent'"},
+            'alt_text': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'caption': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '255'}),
+            'ordering': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'imagecontent_set'", 'to': u"orm['page.Page']"}),
+            'position': ('django.db.models.fields.CharField', [], {'default': "'left'", 'max_length': '10'}),
+            'region': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'page.page': {
+            'Meta': {'ordering': "['tree_id', 'lft']", 'object_name': 'Page'},
+            '_cached_url': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'db_index': 'True', 'blank': 'True'}),
+            '_content_title': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            '_page_title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'in_navigation': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'navigation_extension': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'override_url': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': u"orm['page.Page']"}),
+            'publication_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 6, 27, 0, 0)'}),
+            'publication_end_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'redirect_to': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '150'}),
+            'template_key': ('django.db.models.fields.CharField', [], {'default': "'basic.html'", 'max_length': '255'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        u'page.richtextcontent': {
+            'Meta': {'ordering': "['ordering']", 'object_name': 'RichTextContent', 'db_table': "u'page_page_richtextcontent'"},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ordering': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'richtextcontent_set'", 'to': u"orm['page.Page']"}),
+            'region': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'text': ('feincms.contrib.richtext.RichTextField', [], {'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['page']
