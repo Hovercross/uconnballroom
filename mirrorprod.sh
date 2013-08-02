9027b7a76848723e704220a1846aba2dd30b10b0
Adam Peacock <adam@thepeacock.net>
2013-08-02 13:01:41 -0400

    Added the mirror production script

---
+++ mirrorprod.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+/opt/local/lib/postgresql92/bin/dropdb ballroomcms
+/opt/local/lib/postgresql92/bin/createdb ballroomcms
+
+rsync --delete -r adam@tigger.peacockhosting.net:/var/www/uconnballroom.com/data/cms_user_media/* /Users/adam/development/ballroom-cms/user_media/
+
+ssh adam@tigger.peacockhosting.net 'pg_dump ballroomcms' | /opt/local/lib/postgresql92/bin/psql ballroomcms
