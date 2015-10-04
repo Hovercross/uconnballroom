93b31f6d00bdb9427f01d8cc67b63e201f64eabb
Adam M Peacock <adam@thepeacock.net>
2015-10-04 14:26:05 -0400

    Little fix for redis backend; tabs to spaces

--- dashboard/views/utils.py
+++ dashboard/views/utils.py
@@ -7,11 +7,13 @@ import ballroom.celery
 
 @permission_required("registraiton.change_persontypeautolist")
 def rebuild_managed_lists(request):
-	
-	for worker, tasks in ballroom.celery.app.control.inspect().active().items():
-		for task in tasks:
-			if task["name"] == lists.tasks.rebuildManagedLists.name:
-				return HttpResponse("List rebuild is already in progress. Please wait for current task to complete")
-	
-	lists.tasks.rebuildManagedLists.delay()
-	return HttpResponse("List rebuild has been queued.")
+    
+    activeTasks = ballroom.celery.app.control.inspect().active()
+    if activeTasks:
+        for worker, tasks in activeTasks.items():
+            for task in tasks:
+                if task["name"] == lists.tasks.rebuildManagedLists.name:
+                    return HttpResponse("List rebuild is already in progress. Please wait for current task to complete")
+    
+    lists.tasks.rebuildManagedLists.delay()
+    return HttpResponse("List rebuild has been queued.")
