6012164949bcd6889bc30d064dd096b7fa434eea
Adam M Peacock <adam@thepeacock.net>
2015-10-04 11:40:19 -0400

    Misc fixes, tabs to spaces

--- lists/lib.py
+++ lists/lib.py
@@ -9,112 +9,112 @@ import logging
 logger = logging.getLogger(__name__)
 
 class ListParseException(Exception):
-	def __init__(self, s):
-		self.s = s
-		
-	def __str__(self):
-		return "List parse exception: %s" % self.s
+    def __init__(self, s):
+        self.s = s
+        
+    def __str__(self):
+        return "List parse exception: %s" % self.s
 
 
 def updateListsFor(r):
-	addLists = set()
-	removeLists = set()
-	
-	paidList = autoList('%s Paid' % r.registration_session.card_code, 'paid_list')
-	unpaidList = autoList('%s Unpaid' % r.registration_session.card_code, 'paid_list')
-	teamList = autoList('%s Team' % r.registration_session.card_code, 'registration_type_list')
-	clubList = autoList('%s Club' % r.registration_session.card_code, 'registration_type_list')
-	
-	if r.paid:
-		addLists.add(paidList)
-		removeLists.add(unpaidList)
-	else:
-		addLists.add(unpaidList)
-		removeLists.add(paidList)
-		
-	if r.team:
-		addLists.add(teamList)
-		removeLists.add(clubList)
-	else:
-		addLists.add(clubList)
-		removeLists.add(teamList)
-	
-	#Get the appropriate lists for the person type of the user
-	for apt in registration.models.PersonTypeAutoList.objects.filter(person_type=r.person_type):
-		listName = "%s %s" % (r.registration_session.card_code, apt.list_name)
-		addLists.add(autoList(listName, 'person_type_list'))
-	
-	#Get all the other lists for all the other person types for that registration session, subtracting the lists that the user will now be getting
-	#The case of the person's new list will be handled by the fact that they're already in addLists
-	for l in (set(lists.models.List.objects.filter(list_type='person_type_list').filter(name__startswith=r.registration_session.card_code)) - addLists):
-		removeLists.add(l)
-			
-	for l in removeLists:
-		if r.person in l.people.all():
-			logger.info("Removing person %d from list %s" % (r.person.id, l))
-			l.people.remove(r.person)
-			l.save()
-			
-	for l in addLists:
-		if r.person not in l.people.all():
-			logger.info("Adding person %d from list %s" % (r.person.id, l))
-			l.people.add(r.person)
-			l.save()
+    addLists = set()
+    removeLists = set()
+    
+    paidList = autoList('%s Paid' % r.registration_session.card_code, 'paid_list')
+    unpaidList = autoList('%s Unpaid' % r.registration_session.card_code, 'paid_list')
+    teamList = autoList('%s Team' % r.registration_session.card_code, 'registration_type_list')
+    clubList = autoList('%s Club' % r.registration_session.card_code, 'registration_type_list')
+    
+    if r.paid:
+        addLists.add(paidList)
+        removeLists.add(unpaidList)
+    else:
+        addLists.add(unpaidList)
+        removeLists.add(paidList)
+        
+    if r.team:
+        addLists.add(teamList)
+        removeLists.add(clubList)
+    else:
+        addLists.add(clubList)
+        removeLists.add(teamList)
+    
+    #Get the appropriate lists for the person type of the user
+    for apt in registration.models.PersonTypeAutoList.objects.filter(person_type=r.person_type):
+        listName = "%s %s" % (r.registration_session.card_code, apt.list_name)
+        addLists.add(autoList(listName, 'person_type_list'))
+    
+    #Get all the other lists for all the other person types for that registration session, subtracting the lists that the user will now be getting
+    #The case of the person's new list will be handled by the fact that they're already in addLists
+    for l in (set(lists.models.List.objects.filter(list_type='person_type_list').filter(name__startswith=r.registration_session.card_code)) - addLists):
+        removeLists.add(l)
+            
+    for l in removeLists:
+        if r.person in l.people.all():
+            logger.info("Removing person %d from list %s" % (r.person.id, l))
+            l.people.remove(r.person)
+            l.save()
+            
+    for l in addLists:
+        if r.person not in l.people.all():
+            logger.info("Adding person %d from list %s" % (r.person.id, l))
+            l.people.add(r.person)
+            l.save()
 
 def parseQueryList(s, sep):
-    items = [item.strip() for item in s.split(sep)])
-	
-	opers = {
-		'x': set.intersection,
-		'X': set.intersection,
+    items = [item.strip() for item in s.split(sep)]
+    
+    opers = {
+        'x': set.intersection,
+        'X': set.intersection,
         '&': set.intersection,
-		'+': set.union,
+        '+': set.union,
         '|': set.union,
-		'-': set.difference,
+        '-': set.difference,
         '^': set.symmetric_difference,}
-	
-	stack = []
-	
-	for lineNum, item in enumerate(items):
-		if not item:
-			continue
-			
-		if item in opers:
-			try:
-				op2 = stack.pop()
-				op1 = stack.pop()
-			except IndexError:
-				raise ListParseException("Operand on an empty stack, too many operators")
-			result = opers[item](op1, op2)
-			stack.append(result)
-		else:
-			try:
-				listObj = lists.models.List.objects.get(slug=item)
-			except lists.models.List.DoesNotExist:
-				raise ListParseException("List matching %s not found" % item)
-			stack.append(set(listObj.people.all()))
-			
-	if len(stack) > 1:
-		raise ListParseException("There were leftover lists on the stack")
-	
-	if len(stack) == 0:
-		raise ListParseException("There were no results left on the stack")
-		
-	return stack[0]
-	
+    
+    stack = []
+    
+    for lineNum, item in enumerate(items):
+        if not item:
+            continue
+            
+        if item in opers:
+            try:
+                op2 = stack.pop()
+                op1 = stack.pop()
+            except IndexError:
+                raise ListParseException("Operand on an empty stack, too many operators")
+            result = opers[item](op1, op2)
+            stack.append(result)
+        else:
+            try:
+                listObj = lists.models.List.objects.get(slug=item)
+            except lists.models.List.DoesNotExist:
+                raise ListParseException("List matching %s not found" % item)
+            stack.append(set(listObj.people.all()))
+            
+    if len(stack) > 1:
+        raise ListParseException("There were leftover lists on the stack")
+    
+    if len(stack) == 0:
+        raise ListParseException("There were no results left on the stack")
+        
+    return stack[0]
+    
 def autoList(name, type_code):
-	slug = slugify(name)
+    slug = slugify(name)
 
-	try:
-		return lists.models.List.objects.get(slug=slug)
-	except lists.models.List.DoesNotExist:
-		l = lists.models.List(name=name, slug=slug, list_type=type_code)
-		l.save()
+    try:
+        return lists.models.List.objects.get(slug=slug)
+    except lists.models.List.DoesNotExist:
+        l = lists.models.List(name=name, slug=slug, list_type=type_code)
+        l.save()
 
-		return l
+        return l
 
 def updateListsEvent(sender, instance, **kwargs):
-	updateListsFor(instance)
+    updateListsFor(instance)
 
 #Automatically update a registration's lists on it's save
 post_save.connect(updateListsEvent, sender=registration.models.Registration)
--- registration/admin.py
+++ registration/admin.py
@@ -11,73 +11,74 @@ from registration.models import PersonType, RegistrationSession, PersonEmail, Pe
 from django.template.defaultfilters import slugify
 
 class AutoListPersonTypeInlineAdmin(admin.TabularInline):
-	model = PersonTypeAutoList
-	
+    model = PersonTypeAutoList
+    
 
 
 class PersonTypeAdmin(SortableAdmin):
-	inlines = [AutoListPersonTypeInlineAdmin]
+    inlines = [AutoListPersonTypeInlineAdmin]
 
 
 class EmailAdminForm(forms.ModelForm):
-	class Meta:
-		model = PersonEmail
-		
-	def clean_email(self):
-		return self.cleaned_data["email"].lower()
+    class Meta:
+        model = PersonEmail
+        fields = ['email', 'send']
+        
+    def clean_email(self):
+        return self.cleaned_data["email"].lower()
 
 class InlineEmailAdmin(admin.TabularInline):
-	model = PersonEmail
-	form = EmailAdminForm
-	
+    model = PersonEmail
+    form = EmailAdminForm
+    
 class InlineRegistrationAdmin(admin.TabularInline):
-	model = Registration
-	fields = ['person_type', 'team', 'registered_at', 'amount_due', 'paid_amount', 'sent_registration_email']
-	ordering = ('registration_session__year', '-registration_session__semester')
-	
-	readonly_fields = ('amount_due', 'registered_at', 'paid_amount')
+    model = Registration
+    fields = ['person_type', 'team', 'registered_at', 'amount_due', 'paid_amount', 'sent_registration_email']
+    ordering = ('registration_session__year', '-registration_session__semester')
+    
+    readonly_fields = ('amount_due', 'registered_at', 'paid_amount')
 
-	def has_add_permission(self, request):
-		return False
+    def has_add_permission(self, request):
+        return False
 
-	def has_delete_permission(self, request, obj=None):
-		return False
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 class PersonAdmin(admin.ModelAdmin):
-	inlines = [InlineEmailAdmin, InlineRegistrationAdmin]
-	search_fields = ['first_name', 'last_name']
-	
-	def has_delete_permission(self, request, obj=None):
-		if not request.user.has_perm('registration.delete_person'):
-			return False
+    inlines = [InlineEmailAdmin, InlineRegistrationAdmin]
+    search_fields = ['first_name', 'last_name']
+    
+    def has_delete_permission(self, request, obj=None):
+        if not request.user.has_perm('registration.delete_person'):
+            return False
 
-		if not obj:
-			return request.user.has_perm('registration.delete_person')
+        if not obj:
+            return request.user.has_perm('registration.delete_person')
 
-		for r in obj.registration_set.all():
-			if r.paid_amount > 0:
-				return False
+        for r in obj.registration_set.all():
+            if r.paid_amount > 0:
+                return False
 
-		return request.user.has_perm('registration.delete_person')
+        return request.user.has_perm('registration.delete_person')
 
 class RegistrationSessionAdmin(admin.ModelAdmin):
-	fields = ['year', 'semester', 'card_code', 'base_price', 'team_surcharge', 'nonstudent_surcharge', 'returning_discount', 'early_discount', 'early_deadline', 'first_club_day', 'last_free_day', 'available']
-	
-	list_display = ['year', 'semester', 'available']
+    fields = ['year', 'semester', 'card_code', 'base_price', 'team_surcharge', 'nonstudent_surcharge', 'returning_discount', 'early_discount', 'early_deadline', 'first_club_day', 'last_free_day', 'available']
+    
+    list_display = ['year', 'semester', 'available']
 
-	inlines = [InlineRegistrationAdmin]
+    inlines = [InlineRegistrationAdmin]
 
-	def has_delete_permission(self, request, obj=None):
-		if not request.user.has_perm('registration.delete_registrationsession'):
-			return False
+    def has_delete_permission(self, request, obj=None):
+        if not request.user.has_perm('registration.delete_registrationsession'):
+            return False
 
-		if not obj:
-			return request.user.has_perm('registration.delete_registrationsession')
-	
-		if obj.registration_set.count() > 0:
-			return False
+        if not obj:
+            return request.user.has_perm('registration.delete_registrationsession')
+    
+        if obj.registration_set.count() > 0:
+            return False
 
-		return request.user.has_perm('registration.delete_registrationsession')
+        return request.user.has_perm('registration.delete_registrationsession')
 
 admin.site.register(RegistrationSession, RegistrationSessionAdmin)
 admin.site.register(PersonType, PersonTypeAdmin)
